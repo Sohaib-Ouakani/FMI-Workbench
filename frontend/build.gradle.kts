1bd7f5ef072ad0da2ca7cb8b3e0f8d23e03dff56
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-05-08 21:10:00 +0200

    feat: add dependencies nedeed for graphs and serialisation

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlinxSerialization)
 }
 
 group = "org.findaname"
@@ -34,11 +35,12 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material3)
-                implementation(compose.ui)
+                implementation("org.jetbrains.compose.runtime:runtime:1.11.0-alpha04")
+                implementation("org.jetbrains.compose.foundation:foundation:1.11.0-alpha04")
+                implementation("org.jetbrains.compose.material3:material3:1.9.0")
+                implementation("org.jetbrains.compose.ui:ui:1.11.0-alpha04")
                 implementation(libs.voyager.navigation)
+                implementation(libs.koalaplot.core)
 
                 implementation("io.ktor:ktor-client-core:3.0.3")
                 implementation("io.ktor:ktor-client-content-negotiation:3.0.3")
